2c07b71f889e09246ed4264c46ca777dc33ab680
Junya Wada <s23c1148re@s.chibakoudai.jp>
2025-01-08 18:16:13 +0900

    update receive_position.py

--- satellite_position/receive_position.py
+++ satellite_position/receive_position.py
@@ -5,19 +5,21 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32 
 
 class ListenPositionNode(Node):
     def __init__(self):
         super().__init__('receive_position')
-        self.subscription = self.create_subscription(
-            String,
-            'satellite_position',
-            self.listener_callback,
-            10)
+        self.lat_sub = self.create_subscription(Float32,'satellite_latitude',self.lat_cb,10)
+        self.lon_sub = self.create_subscription(Float32,'satellite_longitude',self.lon_cb,10)
+        self.alt_sub = self.create_subscription(Float32,'satellite_altitude',self.alt_cb,10)
 
-    def listener_callback(self, msg):
-        self.get_logger().info(f'Received satellite position: {msg.data}')
+    def lat_cb(self, msg):
+        self.get_logger().info(f'Latitude: {msg.data}')
+    def lon_cb(self, msg):
+        self.get_logger().info(f'Longitude:{msg.data}')
+    def alt_cb(self, msg):
+        self.get_logger().info(f'Altitude:{msg.data}')
 
 def main(args=None):
     rclpy.init(args=args)
