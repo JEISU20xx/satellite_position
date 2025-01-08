b08f533a34c6bd705b81c251cc18e3fbafcfb97a
Junya Wada <s23c1148re@s.chibakoudai.jp>
2025-01-08 17:50:05 +0900

    Divided into three topics

--- satellite_position/fetch_position.py
+++ satellite_position/fetch_position.py
@@ -7,15 +7,17 @@ import os
 import rclpy
 from rclpy.node import Node
 import requests
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 
 class SatellitePositionNode(Node):
     def __init__(self):
         super().__init__('satellite_position_node')
-        self.pub = self.create_publisher(String, 'satellite_position', 10)
+        self.lat_pub = self.create_publisher(Float32, 'satellite_latitude', 10)
+        self.lon_pub = self.create_publisher(Float32, 'satellite_longitude', 10)
+        self.alt_pub = self.create_publisher(Float32, 'satellite_altitude', 10)
         self.create_timer(5.0, self.cb)
         self.api_key = os.getenv('N2YO_API_KEY')
-        self.satellite_id = '25544'
+        self.satellite_id = '25544' # NORAD ID
 
     def cb(self):
         url = f'https://api.n2yo.com/rest/v1/satellite/positions/{self.satellite_id}/0/0/0/1/&apiKey={self.api_key}'
@@ -23,11 +25,16 @@ class SatellitePositionNode(Node):
         if response.status_code == 200:
             data = response.json()
             positions = data.get('positions', [])
-            if positions:
-                position = positions[0]
-                msg = String()
-                msg.data = f"Lat: {position['satlatitude']}, Lon: {position['satlongitude']}, Alt: {position['sataltitude']}"
-                self. pub.publish(msg)
+            position = positions[0]
+            lat = Float32()
+            lon = Float32()
+            alt = Float32()
+            lat.data = position['satlatitude']
+            lon.data = position['satlongitude']
+            alt.data = position['sataltitude'] # km
+            self.lat_pub.publish(lat)
+            self.lon_pub.publish(lon)
+            self.alt_pub.publish(alt)
 def main():
     rclpy.init()
     node = SatellitePositionNode()
